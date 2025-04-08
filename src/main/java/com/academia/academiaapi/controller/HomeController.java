9f5a3aa769b546441356ee6f9890405a79634eaa
CamilaFPrado <camilapdefarias@gmail.com>
2025-04-08 20:16:27 -0300

    commit homeController

---
+++ src/main/java/com/academia/academiaapi/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.academia.academiaapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 API da Academia está online!";
+    }
+}
+
